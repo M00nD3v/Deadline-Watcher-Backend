045f8bd747a2c35cd85ff7fe5ffbcdc1f8d31fb8
M00nD3v <26168940+M00nD3v@users.noreply.github.com>
2022-09-07 01:04:01 +0200

    added GitHub workflow

---
+++ server-deploy.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+set -e
+
+echo "Deploying application ..."
+
+# Enter maintenance mode
+(php artisan down) || true
+	# Update codebase
+	git fetch origin deploy
+    git reset --hard origin/deploy
+
+	# Install dependencies based on lock file
+    composer install --no-interaction --prefer-dist --optimize-autoloader
+
+	# Migrate database
+	php artisan migrate --force
+
+	# Note: If you're using queue workers, this is the place to restart them.
+    # ...
+
+	# Clear cache
+	php artisan optimize
+
+	# Reload PHP to update opcache
+    echo "" | sudo -S service php8.1-fpm reload # »uberspace tools restart php«
+# Exit maintenance mode
+php artisan up
+
+echo "Application deployed!"
